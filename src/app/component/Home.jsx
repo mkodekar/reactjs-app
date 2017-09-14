5c71ee0b7acdeaa137f5717b3c7b0a0c2b28553d
Rehan Kodekar <rkodekar@zoho.com>
2017-09-14 18:23:36 +0530

    Test React's Virtual dom
    enable paintflasing in your browser to see
    how react handles dom changes.
    
    the status updates on every 3 second interval
    and see how the paintflashing only update the desired field rather than
    refreshing the whole ui.
    
    Signed-off-by: Rehan Kodekar <rkodekar@zoho.com>

--- src/app/component/Home.jsx
+++ src/app/component/Home.jsx
@@ -6,8 +6,10 @@ class Home extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            age: props.user.age 
+            age: props.user.age,
+            status: 0
         }; 
+        
         this.makeMeOlder = this.makeMeOlder.bind(this);
     }
 
@@ -29,12 +31,19 @@ class Home extends React.Component {
         for (let number =0; number < 10; number++) {
             content = <p> Thew number is {number}</p>
         }
+
+        setTimeout(() => {
+            this.setState({
+               status: this.state.status+1
+            });
+        }, 3000);
         return(
             <div>
                 {content}
                 {5 == 3 ? "Yes" : "No"}
                 <h3>Your name is {this.props.user.name}</h3>
                 <p>your age is {this.state.age} {ln}
+                    status = {this.state.status}
                 </p>
                 <h5>Hobbies</h5>
                     <ul>
